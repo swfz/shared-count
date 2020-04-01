d7e181aae69f97db4448f79231bd5df6ccdead0b
swfz <sawafuji.09@gmail.com>
2020-04-01 12:33:50 +0900

    feat: hatena

---
+++ hatena/index.js
@@ -0,0 +1,32 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+
+const fetch = require('node-fetch');
+const {Storage} = require('@google-cloud/storage');
+
+exports.collectHatenaCount = async (event, context) => {
+  const data = Buffer.from(event.data, 'base64').toString();
+  const params = JSON.parse(data);
+
+  const urlBase = 'http://b.hatena.ne.jp/entry/jsonlite/?url=';
+  const requestUrl = `${urlBase}${params.url}`;
+
+  const bucketName = (process.env['NODE_ENV'] == 'production') ? 'memo-raw-data' : 'dev-memo-raw-data';
+  const storage = new Storage();
+
+  const res = await fetch(requestUrl).then((res) => {
+    console.log(res);
+    return res.json();
+  });
+
+  console.log(res);
+
+  const bucket = storage.bucket(bucketName);
+  const file = bucket.file(`share-count/service=${params.service}/${encodeURIComponent(params.url)}`);
+
+  await file.save(JSON.stringify(res));
+};
