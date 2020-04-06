af327121edda7d9edae6dfe6c762e2793edf4a55
swfz <sawafuji.09@gmail.com>
2020-04-06 11:57:30 +0900

    feat: publisherを追加

---
+++ publisher/README.md
@@ -0,0 +1,64 @@
+# publisher
+
+記事リストをもとに各Topicへpublishするための関数
+
+## 記事リスト
+
+JSONLineフォーマット
+
+```
+{"title":"hoge", "url":"https://........", "updated":"2020-04-01", "platform": "blog"},
+{"title":"fuga", "url":"https://........", "updated":"2020-04-02", "platform": "blog"},
+```
+
+## 準備
+### Publisher
+
+- topic
+
+```
+gcloud pubsub topics create memo-collector-publish
+```
+
+- scheduler
+
+```
+gcloud scheduler jobs create pubsub publish-job --schedule="2 * * * *" --topic=memo-collector-publish --message-body="publish"
+```
+
+## development
+
+- environment
+
+環境変数の設定が必要
+
+| name | description |
+|:-|:-|
+| GOOGLE_APPLICATION_CREDENTIALS | credentialファイルのパス |
+| ARTICLES_PATH | 記事リストのjsonのファイルパス |
+| ARTICLES_BUCKET | 記事リストのjsonがあるバケット名 |
+| TOPIC_RREFIX | 各サービスTopic共通のprefix |
+
+
+プロジェクトに合わせて変える
+
+```
+export ARTICLES_PATH=articles/articles.json
+export ARTICLES_BUCKET=memo-raw-data
+export TOPIC_RREFIX=memo-collector-
+npx functions-framework --target=publishToCollector --signature-type=event
+```
+
+- 確認
+
+```
+curl -XPOST -H 'Content-Type:application/json; charset=utf-8' http://localhost:8080/
+```
+
+
+## deploy
+
+```
+gcloud functions deploy publishToCollector --trigger-topic=memo-collector-publish --runtime nodejs10 --region asia-northeast1 --set-env-vars ARTICLES_BUCKET=memo-raw-data,ARTICLES_PATH=articles/articles.json,TOPIC_RREFIX=memo-collector-
+```
+
---
+++ publisher/index.js
@@ -0,0 +1,74 @@
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+
+const {Storage} = require('@google-cloud/storage');
+const {PubSub} = require('@google-cloud/pubsub');
+const fs = require('fs');
+
+exports.publishToCollector = async (event, context) => {
+  const services = ['twitter', 'pocket', 'facebook', 'hatena'];
+  const articles = await getArticles();
+
+  const target = filterTargetArticles(articles, (new Date()).getHours());
+
+  const pubsub = new PubSub();
+  const topicPrefix = process.env['TOPIC_PREFIX'];
+
+  for (let i = 0; i < services.length; i++) {
+    const service = services[i];
+    const topic = `${topicPrefix}${service}`;
+    const publisher = pubsub.topic(topic, {batching: { maxMilliseconds: 10000}});
+
+    for (let j = 0; j < target.length; j++) {
+      const url = target[j].url;
+      const data = {url, service};
+
+      const dataBuffer = Buffer.from(JSON.stringify(data));
+      const messageId = await publisher.publish(dataBuffer);
+      console.log(`[${topic}][${url}]: Message ${messageId} published.`);
+    }
+  }
+};
+
+const filterTargetArticles = (articles, hour) => {
+  const denominator = 24;
+
+  const filterdArticles = articles.filter(a => !a.hasOwnProperty('platform') || a.platform == 'blog');
+
+  const sortFn = (a,b) => {
+    if(a.hasOwnProperty('updated')){
+      if(a.updated < b.updated) return 1;
+      if(a.updated > b.updated) return -1;
+      return 0;
+    }
+    else {
+      if (a.title < b.title) return 1;
+      if (a.title > b.title) return -1;
+      return 0;
+    }
+  }
+  const sortedArticles = filterdArticles.sort(sortFn);
+
+  const unit = Math.ceil(sortedArticles.length / denominator);
+  const start = unit * hour;
+  const end = start + unit;
+
+  return sortedArticles.slice(start, end);
+}
+
+const getArticles = async () => {
+  const bucketName = process.env['ARTICLES_BUCKET'];
+  const fileName = process.env['ARTICLES_PATH']
+  const storage = new Storage();
+
+  await storage.bucket(bucketName).file(fileName).download({destination: './articles.json'});
+
+  const articlesJSONLines = fs.readFileSync('./articles.json', 'utf8');
+  const json = `[${articlesJSONLines.replace(/\n/gi,',').replace(/,$/,'')}]`;
+
+  return JSON.parse(json);
+}
