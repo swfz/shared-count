4977d7c8468fe6eea8e3ffe215267b01b70b4dee
swfz <sawafuji.09@gmail.com>
2020-05-01 02:34:53 +0900

    chore: 正常系動作確認

--- gs-lt.sh
+++ gs-lt.sh
@@ -5,7 +5,7 @@ code=0
 while read line
 do
   updated=`echo $line | awk '{print $2}' | tr -d 'Z' | tr 'T' ' ' | date +"%Y-%m-%dT%H:%M:%S" -f -`
-  last2day=`date --utc -d '20 hour ago' +"%Y-%m-%dT%H:%M:%S"`
+  last2day=`date --utc -d '2 day ago' +"%Y-%m-%dT%H:%M:%S"`
 
   if [[ "${updated}" < "${last2day}" ]]; then
     echo $line >&2
