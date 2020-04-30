62e301534025c1d62ad02d97cbe9e0da59d4ea0b
swfz <sawafuji.09@gmail.com>
2020-05-01 02:33:06 +0900

    fix: read lineの中で変数を変更できるようにした

--- gs-lt.sh
+++ gs-lt.sh
@@ -2,16 +2,15 @@
 
 code=0
 
-cat - | while read line
+while read line
 do
   updated=`echo $line | awk '{print $2}' | tr -d 'Z' | tr 'T' ' ' | date +"%Y-%m-%dT%H:%M:%S" -f -`
-  last1day=`date --utc -d '10 hour ago' +"%Y-%m-%dT%H:%M:%S"`
+  last2day=`date --utc -d '20 hour ago' +"%Y-%m-%dT%H:%M:%S"`
 
-  if [[ "${updated}" < "${last1day}" ]]; then
+  if [[ "${updated}" < "${last2day}" ]]; then
     echo $line >&2
     code=1
   fi
-done
+done < <(cat -)
 
 exit ${code}
-
