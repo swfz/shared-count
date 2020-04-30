a8a58c127ca23e346de13b92416ed854a2611481
swfz <sawafuji.09@gmail.com>
2020-04-30 18:27:04 +0900

    chore: 動作確認

--- gs-lt.sh
+++ gs-lt.sh
@@ -3,7 +3,7 @@
 cat - | while read line
 do
   updated=`echo $line | awk '{print $2}' | tr -d 'Z' | tr 'T' ' ' | date +"%Y-%m-%dT%H:%M:%S" -f -`
-  last1day=`date --utc -d '2 day ago' +"%Y-%m-%dT%H:%M:%S"`
+  last1day=`date --utc -d '10 hour ago' +"%Y-%m-%dT%H:%M:%S"`
 
   if [[ "${updated}" < "${last1day}" ]]; then
     echo $line >&2
